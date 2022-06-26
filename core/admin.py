7d2bf8c33ba4f0225c3836c2bf1c323e75b026da
Marina <petrukmn@yandex.ru>
2022-06-26 16:10:32 +0300

    Step #1. Create db, user model, add admin settings

---
+++ core/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from core.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    exclude = ('password', )
+    readonly_fields = ('last_login', 'date_joined')
+    list_display = ('username', 'email', 'first_name', 'last_name', 'password')
+    list_display_links = ('username',)
+    list_filter = ('is_staff', 'is_active', 'is_superuser')
+    search_fields = ('email', 'first_name', 'last_name', 'username')
+    list_editable = ('password', )
