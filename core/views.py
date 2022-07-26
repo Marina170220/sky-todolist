b1847d72ef5b11f71436c4df4c67ee3bb1552e61
Marina <petrukmn@yandex.ru>
2022-07-26 17:06:44 +0300

    Step #3. Add ensure_csrf_cookie decorator to ProfileView

--- core/views.py
+++ core/views.py
@@ -37,7 +37,7 @@ class ProfileView(RetrieveUpdateDestroyAPIView):
     permission_classes = [IsAuthenticated]
     serializer_class = UserSerializer
 
-    # @method_decorator(ensure_csrf_cookie)
+    @method_decorator(ensure_csrf_cookie)
     def get_object(self):
         return self.request.user
 
