9aa0fa7a63dd719dbc3ea192a7de86014c7dd7f2
Marina <petrukmn@yandex.ru>
2022-07-30 21:28:41 +0300

    Step #4. Add goals and comments views, urls. Fix goal admin

--- goals/admin.py
+++ goals/admin.py
@@ -1,11 +1,24 @@
 from django.contrib import admin
 
-from goals.models import Category
+from goals.models import Category, Goal, Comment
 
-
+@admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ("title", "user", "created", "updated")
+    list_display = ("title", "user", "created", "updated", "is_deleted")
+    list_display_links = ("title", )
+    list_filter = ('user', 'created')
+    search_fields = ("title",)
+
+@admin.register(Goal)
+class GoalAdmin(admin.ModelAdmin):
+    list_display = ("title", "category", "user", "status", "created", "updated", "priority", "expires")
+    list_display_links = ("title", )
+    list_filter = ('user', 'expires', 'category', 'status', 'priority')
     search_fields = ("title", "user")
 
-
-admin.site.register(Category, CategoryAdmin)
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("text", "user", "goal", "created", "updated")
+    list_display_links = ("text", )
+    list_filter = ('user', 'goal', 'created')
+    search_fields = ("text", "user")
--- goals/views.py
+++ goals/views.py
@@ -4,8 +4,11 @@ from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveUpdateDe
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import IsAuthenticated
 
-from goals.models import Category
-from goals.serializers import CategoryCreateSerializer , CategorySerializer
+from goals.filters import GoalDateFilter
+from goals.models import Category, Goal, Status, Comment
+from goals.permissions import CommentPermissions
+from goals.serializers import CategoryCreateSerializer, CategorySerializer, GoalCreateSerializer, GoalSerializer, \
+    CommentCreateSerializer, CommentSerializer
 
 
 class CategoryCreateView(CreateAPIView):
@@ -41,3 +44,66 @@ class CategoryView(RetrieveUpdateDestroyAPIView):
         instance.is_deleted = True
         instance.save()
         return instance
+
+
+class GoalCreateView(CreateAPIView):
+    model = Goal
+    permission_classes = [IsAuthenticated]
+    serializer_class = GoalCreateSerializer
+
+
+class GoalListView(ListAPIView):
+    model = Goal
+    permission_classes = [IsAuthenticated]
+    serializer_class = GoalSerializer
+    pagination_class = LimitOffsetPagination
+    filter_backends = [DjangoFilterBackend, OrderingFilter, SearchFilter]
+    filterset_fields = GoalDateFilter
+    ordering_fields = ["priority", "expires"]
+    ordering = ["priority", "expires"]
+    search_fields = ["title", "description"]
+
+    def get_queryset(self):
+        return Goal.objects.filter(user=self.request.user)
+
+
+class GoalView(RetrieveUpdateDestroyAPIView):
+    model = Goal
+    serializer_class = GoalSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Goal.objects.filter(user=self.request.user)
+
+    def perform_destroy(self, instance):
+        instance.status = Status.archived
+        instance.save()
+        return instance
+
+
+class CommentCreateView(CreateAPIView):
+    model = Comment
+    permission_classes = [IsAuthenticated]
+    serializer_class = CommentCreateSerializer
+
+
+class CommentListView(ListAPIView):
+    model = Comment
+    permission_classes = [IsAuthenticated, CommentPermissions]
+    serializer_class = CommentSerializer
+    pagination_class = LimitOffsetPagination
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ["goal"]
+    ordering = ["-id"]
+
+    def get_queryset(self):
+        return Comment.objects.filter(user=self.request.user)
+
+
+class CommentView(RetrieveUpdateDestroyAPIView):
+    model = Comment
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated, CommentPermissions]
+
+    def get_queryset(self):
+        return Comment.objects.filter(user=self.request.user)
