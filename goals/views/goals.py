006076495fe917115086e6bd05098a8ec4c43bf0
Marina <petrukmn@yandex.ru>
2022-08-04 09:55:25 +0300

    Step #5. Fix GoalDateFilter

--- goals/views/goals.py
+++ goals/views/goals.py
@@ -25,7 +25,7 @@ class GoalListView(ListAPIView):
     search_fields = ["title", "description"]
 
     def get_queryset(self):
-        return Goal.objects.filter(user=self.request.user)
+        return Goal.objects.filter(category__board__participants__user=self.request.user)
 
 
 class GoalView(RetrieveUpdateDestroyAPIView):
@@ -34,7 +34,7 @@ class GoalView(RetrieveUpdateDestroyAPIView):
     permission_classes = [GoalPermissions]
 
     def get_queryset(self):
-        return Goal.objects.filter(user=self.request.user)
+        return Goal.objects.filter(category__board__participants__user=self.request.user)
 
     def perform_destroy(self, instance):
         instance.status = Status.ARCHIVED
